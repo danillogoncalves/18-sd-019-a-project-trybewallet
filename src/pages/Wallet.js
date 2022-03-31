35d02757d6e11620eb60f7683b18fddad97dbdd0
Danillo Gonçalves Batista <danillogoncalves001@gmail.com>
2022-03-31 19:55:57 -0300

    :heavy_check_mark: feat: Requisite two finished.

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,9 +1,39 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 class Wallet extends React.Component {
   render() {
-    return <div>TrybeWallet</div>;
+    const { userEmail } = this.props;
+    return (
+      <div>
+        <h1>TrybeWallet</h1>
+        <span
+          data-testid="email-field"
+        >
+          { userEmail }
+        </span>
+        <span
+          data-testid="total-field"
+        >
+          0
+        </span>
+        <span
+          data-testid="header-currency-field"
+        >
+          BRL
+        </span>
+      </div>
+    );
   }
 }
 
-export default Wallet;
+const mapStateToProps = (state) => ({
+  userEmail: state.user.email,
+});
+
+Wallet.propTypes = {
+  userEmail: PropTypes.string.isRequired,
+};
+
+export default connect(mapStateToProps)(Wallet);
